1427022c06d54a7f41280d22904cf82a4f240d08
AleksasN <aleksasne@gmail.com>
2023-01-31 13:06:30 +0200

    fix some troubles

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -36,7 +36,7 @@ const TopChartCard = ({
         <Link to={`/songs/${song.key}`}>
           <p className="text-xl font-bold text-white">{song?.title}</p>
         </Link>
-        <Link to={`/artists/${song?.artists[0].adamid}`}>
+        <Link to={`/artists/${song?.artists?.adamid}`}>
           <p className="text-base text-gray-300 mt-1">{song?.subtitle}</p>
         </Link>
       </div>
@@ -117,7 +117,7 @@ const TopPlay = () => {
               style={{ width: "25%", height: "auto" }}
               className="shadow-lg rounded-full animate-slideright"
             >
-              <Link to={`/artists/${artist?.artists[0].adamid}`}>
+              <Link to={`/artists/${artist?.artists?.adamid}`}>
                 <img
                   src={artist?.images?.background}
                   alt="Name"
